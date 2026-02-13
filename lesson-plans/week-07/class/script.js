ce394f1569c51cab36d32af859743f220a8770d4
iisce <iscedigitalconcept@gmail.com>
2026-02-13 17:22:25 +0100

    feat: class example

---
+++ lesson-plans/week-07/class/script.js
@@ -0,0 +1,6 @@
+document.getElementById('output').innerHTML = '<h4>Hello World!</h4>';
+document.getElementById('output').innerHTML += '<p>This is a paragraph.</p>';
+document.getElementById('output').innerHTML +=
+	'<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>';
+document.getElementById('output').style.color = 'blue';
+document.getElementById('output').style.fontSize = '20px';
